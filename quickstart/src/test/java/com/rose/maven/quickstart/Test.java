204487b763cea567b869979728794e1c7e41b45e
Ekkehard Rose <rose.ekkehard@t-online.de>
2018-10-30 22:16:20 +0100

    new repo

---
+++ quickstart/src/test/java/com/rose/maven/quickstart/Test.java
@@ -0,0 +1,12 @@
+package com.rose.maven.quickstart;
+
+import com.toedter.calendar.JCalendar;
+
+public class Test {
+
+	public static void main(String[] args) {
+		JCalendar jCal = new JCalendar();
+
+	}
+
+}
